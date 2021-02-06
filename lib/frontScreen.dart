8f7167e2e4809090539dd2a8842fef03eda1733b
satyagovind <msatyagovind@gmail.com>
2021-02-06 12:24:21 +0530

    FrontScreen Images,countries_names

--- lib/frontScreen.dart
+++ lib/frontScreen.dart
@@ -18,25 +18,122 @@ class _FrontScreenState extends State<FrontScreen> {
 
   List<FrontSites> editorsSites = [
     FrontSites(
-      name: 'dolomite',
+      name: 'Dolomite',
       location: 'Italy',
-      imageUrl: 'assets/dola.jpg',
-      fact: 'great',
+      imageUrl: 'assets/dolamites.jpg',
+      fact: 'They Were The Site Of Some Of World War I’s Fiercest Battles',
       idNumber: '1237',
     ),
     FrontSites(
-      name: 'Taj Mahal',
-      location: 'Italy',
-      imageUrl: 'assets/1.jpg',
-      idNumber: '252',
-      fact: 'gre',
+      name: 'Ajantha Caves',
+      location: 'India',
+      imageUrl: 'assets/Ajantha_Caves.jpg',
+      idNumber: '242',
+      fact:
+      'Though the caves are over 2,000 years old, the Buddha statues had been added close 600 years later.',
     ),
     FrontSites(
-      name: 'great barrier reef',
-      location: 'Italy',
-      imageUrl: 'assets/2.jpg',
+      name: 'Great Barrier Reef',
+      location: 'Australia',
+      imageUrl: 'assets/Greatbarrierreef.jpg',
       idNumber: '154',
-      fact: 'gret',
+      fact:
+      ' It’s the only place on earth where two UNESCO World Heritage Sites meet. ',
+    ),
+    FrontSites(
+      name: 'Mount Fuji',
+      location: 'Japan',
+      imageUrl: 'assets/Mount_Fuiji.jpg',
+      idNumber: '1418',
+      fact: 'MT FUJI IS ACTUALLY A STRATOVOLCANO! ',
+    ),
+    FrontSites(
+      name: 'Stonehenge',
+      location: 'England',
+      imageUrl: 'assets/Stonehenge.jpg',
+      idNumber: '373',
+      fact: 'IN ITS EARLY DAYS, STONEHENGE WAS A CEMETERY.  ',
+    ),
+    FrontSites(
+      name: 'Grand Canyon',
+      location: 'United States',
+      imageUrl: 'assets/GrandCanyon.jpg',
+      idNumber: '1094',
+      fact: 'MT FUJI IS ACTUALLY A STRATOVOLCANO! ',
+    ),
+    FrontSites(
+      name: 'Acropolis of Athens',
+      location: 'Greece',
+      imageUrl: 'assets/Acroplis-Athens.jpg',
+      idNumber: '404',
+      fact: 'THE WORLD’S OLDEST WEATHER STATION IS AT ITS BASE.',
+    ),
+    FrontSites(
+      name: 'Himeji Castle',
+      location: 'Japan',
+      imageUrl: 'assets/Himejicastle.jpg',
+      idNumber: '661',
+      fact:
+      'For over 400 years, Himeji Castle has remained intact, even throughout the extensive bombing of Himeji in World War II and natural disasters such as the 1995 Great Hanshin earthquake and various typhoons',
+    ),
+  ];
+
+  List<FrontSites> sevenWonders = [
+    FrontSites(
+      name: 'Taj Mahal',
+      location: 'India',
+      imageUrl: 'assets/Tajmahal.jpg',
+      fact:
+      'The tomb contains 99 different names of Allah as calligraphic inscriptions',
+      idNumber: '252',
+    ),
+    FrontSites(
+      name: 'Great Wall Of China',
+      location: 'China',
+      imageUrl: 'assets/Thegreatwallofchina.jpg',
+      idNumber: '438',
+      fact:
+      'During the Cultural Revolution (1966–1976), many Great Wall bricks were used in building homes, farms, or reservoirs.',
+    ),
+    FrontSites(
+      name: 'Machu Picchu',
+      location: 'Peru',
+      imageUrl: 'assets/Machupichu.jpg',
+      idNumber: '274',
+      fact:
+      'Machu Picchu is made up of more than 150 buildings ranging from baths and houses to temples and sanctuaries. ',
+    ),
+    FrontSites(
+      name: 'Chichen Itza',
+      location: 'Mexico',
+      imageUrl: 'assets/Chichenitza.jpg',
+      idNumber: '483',
+      fact:
+      'It is believed Itza means water magicians, deriving from the Mayan Itz for magic and a for water ',
+    ),
+    FrontSites(
+      name: 'Colosseum',
+      location: 'Rome',
+      imageUrl: 'assets/ColosseumRome.jpg',
+      idNumber: '91',
+      fact:
+      'The arena had 36 trap doors for special effects, as well as many underground passages and rooms to hold wild animals and gladiators before the games began ',
+    ),
+    FrontSites(
+      name: 'Petra',
+      location: 'Jordan',
+      imageUrl: 'assets/Petra.jpg',
+      idNumber: '326',
+      fact:
+      'A Swiss explorer called Johann Ludwig Burckhardt discovered Petra in 1812. Because it was an unknown metropolitan for around 5 centuries, it is also called the ‘Lost City’.',
+    ),
+    FrontSites(
+      name: 'Christ The Redemeer',
+      location: 'Brazil',
+      imageUrl: 'assets/christredemeer.jpg',
+      idNumber: '1100',
+      fact:
+      ' The original design of the statue was different to what we see today. It was intended for Christ to be holding a globe in one hand and a cross in the other, rather than two open arms. ',
     ),
   ];
 
@@ -222,15 +319,11 @@ class _FrontScreenState extends State<FrontScreen> {
               ),
             ),
             FrontScreenSlider(
-              editorsSites: editorsSites,
+              editorsSites: sevenWonders ,
               unescoSites: unescoSites,
-              title: 'Rakesh\'s Pics:',
+              title: 'Seven Wonders:',
             ),
-            FrontScreenSlider(
-              editorsSites: editorsSites,
-              unescoSites: unescoSites,
-              title: 'Govind\'s Pics:',
-            ),
+            Padding(padding:EdgeInsets.all(40) ),
           ],
         ),
       ),
@@ -271,7 +364,7 @@ class FrontScreenSlider extends StatelessWidget {
           CarouselSlider(
             options: CarouselOptions(
               height: 230,
-              // autoPlay: true,
+               autoPlay: true,
             ),
             items: editorsSites
                 .map(
@@ -306,9 +399,24 @@ class FrontScreenSlider extends StatelessWidget {
                                 child: Padding(
                                   padding: const EdgeInsets.only(
                                       top: 20.0, left: 15),
-                                  child: Text(
-                                    e.fact,
-                                    style: TextStyle(color: Colors.black),
+                                  child: RichText(
+                                    text: TextSpan(
+                                      text: e.name + ', ',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                      children: [
+                                        TextSpan(
+                                          text: e.location,
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.normal,
+                                              fontSize: 14),
+                                        )
+                                      ],
+                                    ),
+                                    // style: TextStyle(color: Colors.black),
                                   ),
                                 ),
                               ),
